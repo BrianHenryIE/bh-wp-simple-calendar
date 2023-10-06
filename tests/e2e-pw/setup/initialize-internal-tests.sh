11cae419eb2bd5d1d3ec07652ca6ff79a7595698
Brian Henry <BrianHenryIE@gmail.com>
2023-10-06 14:41:56 -0700

    Add `wp-env` and `Playwright`

---
+++ tests/e2e-pw/setup/initialize-internal-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Print the script name.
+echo $(basename "$0")
+
+echo "Installing latest build of bh-wp-simple-calendar"
+wp plugin install ./setup/bh-wp-simple-calendar.latest.zip --activate --force
