9c5c6b43f62610de9ea59a925cb7e77566659c71
Brian Henry <BrianHenryIE@gmail.com>
2023-10-06 22:04:47 -0700

    Lint

--- tests/wpunit/frontend/class-widget-Test.php
+++ tests/wpunit/frontend/class-widget-Test.php
@@ -6,30 +6,29 @@
  * @author Brian Henry <BrianHenryIE@gmail.com>
  */
 
+use BrianHenryIE\WP_Simple_Calendar\Frontend\Renderer;
 
 /**
  * @coversDefaultClass \BrianHenryIE\WP_Simple_Calendar\Frontend\Widget
  */
 class Widget_Test extends \Codeception\TestCase\WPTestCase {
 
-	public function test_things_start() {
+	public function test_things_start(): void {
 
-		$api = null;
+		$renderer = self::make( Renderer::class );
 
-		$widget = new \BrianHenryIE\WP_Simple_Calendar\Frontend\Widget( $api );
-
-		echo 'widget id: ' . $widget->id;
+		$widget = new \BrianHenryIE\WP_Simple_Calendar\Frontend\Widget( $renderer );
 
 		$this->assertNotNull( $widget );
 	}
 
-	private function an_instance() {
+	protected function an_instance(): array {
 
 		$instance = array(
 			'title'             => '',
 			'event_count'       => '5',
-			'event_period'      => '366', // Days of events to show
-			'cache_time'        => '60', // Minutes until refresh
+			'event_period'      => '366', // Days of events to show.
+			'cache_time'        => '60', // Minutes until refresh.
 			'calendar_id'       => '',
 			'dateformat_lg'     => 'l jS \of F',
 			'suffix_lg_class'   => '',
@@ -40,8 +39,8 @@ class Widget_Test extends \Codeception\TestCase\WPTestCase {
 		return $instance;
 	}
 
-	public function test_widget_output() {
+	public function test_widget_output(): void {
 
-		// Pre-populate the transient for WordPress to pull
+		// Pre-populate the transient for WordPress to pull.
 	}
 }
