d53fa6269db9884737537b29b918d1bc36106ec2
Brian Henry <BrianHenryIE@gmail.com>
2023-10-06 23:54:00 -0700

    Create class-activator-Unit-Test.php

---
+++ tests/unit/wp-includes/class-activator-Unit-Test.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BrianHenryIE\WP_Simple_Calendar\WP_Includes;
+
+/**
+ * @coversDefaultClass \BrianHenryIE\WP_Simple_Calendar\WP_Includes\Activator
+ */
+class Activator_Unit_Test extends \Codeception\Test\Unit {
+
+	protected function setUp(): void {
+		parent::setUp();
+		\WP_Mock::setUp();
+	}
+
+	protected function tearDown(): void {
+		parent::tearDown();
+		\WP_Mock::tearDown();
+	}
+
+	/**
+	 * @covers ::activate
+	 */
+	public function test_activate(): void {
+
+		\WP_Mock::userFunction(
+			'wp_schedule_event',
+			array(
+				'args'  => array(
+					\WP_Mock\Functions::type( 'int' ),
+					'hourly',
+					'bh_wp_update_calendar_caches',
+				),
+				'times' => 1,
+			)
+		);
+
+		Activator::activate();
+	}
+}
