9c2588101ac5f983526f705cfe5c0e4d24523913
Brian Henry <BrianHenryIE@gmail.com>
2026-02-20 19:02:28 -0800

    Create class-cli.php

---
+++ src/wp-includes/class-cli.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BrianHenryIE\WP_Simple_Calendar\WP_Includes;
+
+class CLI {
+
+	// List calendars (print post ids - one-to-many) (print last fetch time)
+	// List calendars posts (print calendars)
+	// Update calendar cache (print list new events)
+
+}
