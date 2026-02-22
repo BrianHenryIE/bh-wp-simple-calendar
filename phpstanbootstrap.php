44bbd401a4afa224f9aade9f3919445bdc087ce9
Brian Henry <BrianHenryIE@gmail.com>
2026-02-21 17:43:32 -0800

    Update phpstanbootstrap.php

--- phpstanbootstrap.php
+++ phpstanbootstrap.php
@@ -7,5 +7,5 @@
  * @package PHP_Package_Name
  */
 
-define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
-define( 'WP_PLUGIN_DIR', __DIR__ . '/wp-content/plugins' );
+//define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
+//define( 'WP_PLUGIN_DIR', __DIR__ . '/wp-content/plugins' );
