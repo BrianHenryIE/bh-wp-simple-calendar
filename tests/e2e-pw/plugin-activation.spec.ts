21fe276508cfb28d25408db236c189ae853d6e52
Brian Henry <BrianHenryIE@gmail.com>
2026-05-22 07:38:21 -0700

    Update plugin-activation.spec.ts

--- tests/e2e-pw/plugin-activation.spec.ts
+++ tests/e2e-pw/plugin-activation.spec.ts
@@ -1,19 +1,37 @@
 /**
  * Verify the plugin activates correctly in wp-env.
  */
+import { Page } from '@playwright/test';
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
-import {loginAsAdmin} from "./helpers/ui/login";
+import { loginAsAdmin } from "./helpers/ui/login";
 
 test.describe( 'Plugin activation', () => {
+
+	async function goToPluginsPage( { admin, page } ): Promise< void > {
+		await loginAsAdmin(page);
+		await admin.visitAdminPage( 'plugins.php' );
+	}
+
+	test.beforeEach( async ( { browser, requestUtils } ) => {
+		// const page = await browser.newPage();
+
+		// Activate plugin
+		await requestUtils.activatePlugin("simple-calendar");
+	} );
+
 	test( 'plugin is listed on the plugins page', async ( { admin, page } ) => {
 
-		await loginAsAdmin(page);
-
-		await admin.visitAdminPage( 'plugins.php' );
+		await goToPluginsPage( { admin, page } );
 
 		const pluginRow = page.locator( 'tr[data-plugin="bh-wp-simple-calendar/bh-wp-simple-calendar.php"]' );
 		await expect( pluginRow ).toBeVisible();
+	} );
 
+	test( 'plugin is active', async ( { admin, page } ) => {
+
+		await goToPluginsPage( { admin, page } );
+
+		const pluginRow = page.locator( 'tr[data-plugin="bh-wp-simple-calendar/bh-wp-simple-calendar.php"]' );
 		const deactivateLink = pluginRow.locator( 'a:has-text("Deactivate")' );
 		await expect( deactivateLink ).toBeVisible();
 	} );
